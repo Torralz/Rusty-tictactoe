52730b3370a378f6dd588c2ad390c1b67b7c24d9
Torralz <pmario.ceatorralba@gmail.com>
2025-08-27 20:27:37 +0200

    Detector de solución completado

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,10 @@ fn main() {
                 continue;
             }
         };
-
+        let ganadorCasilla: Casilla;
         relleno_matriz(&mut matriz, input, Casilla::Cruz);
-        ia(&mut matriz);
+        ia2(&mut matriz);
+        (terminado, ganadorCasilla) = comprobar_ganador(&matriz);
         dibujo_matriz(&matriz);
     }
 }
@@ -124,4 +125,130 @@ fn ia(matriz: &mut [[char; 3]; 3]){
             }
         }
     }
+}
+
+fn ia2(matriz: &mut [[char; 3]; 3]){
+    let mut encontrado: bool = false;
+    for x in 0..3{
+        for y in 0..3{
+            if matriz[x][y] == 'O'{
+                encontrado = true;
+                if x > 0 && y > 0{
+                    for m in x-1..=x+1{
+                        for n in y-1..=y+1{
+                            if (0..3).contains(&m) && (0..3).contains(&n){
+                                if matriz[m][n] == ' '{
+                                    matriz[m][n] = 'O';
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                } 
+                else{
+                    for m in x..=x+1{
+                        for n in y..=y+1{
+                            if (0..3).contains(&m) && (0..3).contains(&n){
+                                if matriz[m][n] == ' '{
+                                    matriz[m][n] = 'O';
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if !encontrado{
+        let mut rng = rand::thread_rng(); // Corregido: usar thread_rng()
+        if matriz[1][1] == ' '{
+            matriz[1][1] = 'O';
+        }else{
+            loop {
+                let fila: usize = rng.gen_range(0..3); // Cambiado a usize y rango 0..3
+                let colu: usize = rng.gen_range(0..3); // Cambiado a usize y rango 0..3
+                if matriz[fila][colu] == ' ' {
+                    matriz[fila][colu] = 'O'; // La IA juega con 'O'
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn comprobar_ganador (matriz: & [[char; 3];3]) -> (bool, Casilla){
+    let solFicha: Casilla;
+    //primero comprobamos las horizontales
+    if matriz[0][0] == matriz[0][1] && matriz[0][2] == matriz[0][1] && matriz[0][1] != ' ' {
+        match matriz[0][0] {
+            'X' => solFicha = Casilla::Cruz,
+            'O' => solFicha = Casilla::Circulo,
+            _ => todo!(),
+        }
+        return (true, solFicha); 
+    }
+    if matriz[1][0] == matriz[1][1] && matriz[1][2] == matriz[1][1] && matriz[1][1] != ' ' {
+        match matriz[1][0] {
+            'X' => solFicha = Casilla::Cruz,
+            'O' => solFicha = Casilla::Circulo,
+            _ => todo!(),
+        }
+        return (true, solFicha); 
+    }
+    if matriz[2][0] == matriz[2][1] && matriz[2][2] == matriz[2][1] && matriz[2][1] != ' ' {
+        match matriz[2][0] {
+            'X' => solFicha = Casilla::Cruz,
+            'O' => solFicha = Casilla::Circulo,
+            _ => todo!(),
+        }
+        return (true, solFicha); 
+    }
+
+    //Seguimos comprobando las Verticales
+    if matriz[0][0] == matriz[1][0] && matriz[2][0] == matriz[1][0] && matriz[1][0] != ' ' {
+        match matriz[0][0] {
+            'X' => solFicha = Casilla::Cruz,
+            'O' => solFicha = Casilla::Circulo,
+            _ => todo!(),
+        }
+        return (true, solFicha); 
+    }
+    if matriz[0][1] == matriz[1][1] && matriz[2][1] == matriz[1][1] && matriz[1][1] != ' ' {
+        match matriz[0][1] {
+            'X' => solFicha = Casilla::Cruz,
+            'O' => solFicha = Casilla::Circulo,
+            _ => todo!(),
+        }
+        return (true, solFicha); 
+    }
+    if matriz[0][2] == matriz[1][2] && matriz[2][2] == matriz[1][2] && matriz[1][2] != ' ' {
+        match matriz[0][2] {
+            'X' => solFicha = Casilla::Cruz,
+            'O' => solFicha = Casilla::Circulo,
+            _ => todo!(),
+        }
+        return (true, solFicha); 
+    }
+
+    //Para terminar comprobamos las diagonales
+    if matriz[0][0] == matriz[1][1] && matriz[2][2] == matriz[1][1] && matriz[1][1] != ' ' {
+        match matriz[0][0] {
+            'X' => solFicha = Casilla::Cruz,
+            'O' => solFicha = Casilla::Circulo,
+            _ => todo!(),
+        }
+        return (true, solFicha); 
+    }
+    if matriz[0][2] == matriz[1][1] && matriz[2][0] == matriz[1][1] && matriz[1][1] != ' ' {
+        match matriz[0][2] {
+            'X' => solFicha = Casilla::Cruz,
+            'O' => solFicha = Casilla::Circulo,
+            _ => todo!(),
+        }
+        return (true, solFicha); 
+    }
+
+
+    (false, Casilla::Vacio)
 }
